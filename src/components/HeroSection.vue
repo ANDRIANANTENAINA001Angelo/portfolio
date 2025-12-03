002f57413e649c52747cb1f2502eb485465f654a
ANDRIANANTENAINA001Angelo <mornigstar25angelo@gmail.com>
2025-12-03 17:48:43 +0300

    test jenkins

--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -19,7 +19,7 @@ onMounted(() => {
 
 useSeo({
   title: `ANDRIANANTENAINA Angelo | Développeur Web & IA à Madagascar`,
-  description: "ANDRIANANTENAINA Angelo, développeur logiciel & IA à Madagascar. Conception et création d’applications, analyse data et automatisation.",
+  description: "ANDRIANANTENAINA Angelo, développeur logiciel & Intelligence Artificielle à Madagascar. Conception et création d’applications, analyse data et automatisation.",
 })
 
 
@@ -59,7 +59,7 @@ const downloadCV = () => {
           <h1 class="hero-title name-highlight">ANDRIANANTENAINA Angelo
             <!-- <span class="name-highlight"></span> -->
           </h1>
-          <h2 class="hero-subtitle">Développeur Logiciel & Intelligence Artificielle</h2>
+          <h2 class="hero-subtitle">Développeur Logiciel & IA</h2>
           <p class="hero-description">
             Basé à Madagascar, je développe des solutions web, mobile et intelligentes. 
             Laravel, Python, IA, automatisation... tout ce qu'il faut pour transformer vos idées en applications concrètes.
